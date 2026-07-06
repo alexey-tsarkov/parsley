08ca70777ea9f6ed44900b70896f46c02ae8c6c7
Alex Tsarkov <alex.tsarkov@gmail.com>
2026-07-06 20:12:33 +0300

    feat: add facade

---
+++ src/Parsley.php
@@ -0,0 +1,80 @@
+<?php
+
+declare(strict_types=1);
+
+namespace AlexTsarkov\Parsley;
+
+abstract class Parsley
+{
+    /**
+     * @template T
+     * @return Parser<T, never>
+     */
+    final public static function fail(): Parser
+    {
+        static $parser = new Parser\FailParser();
+
+        return $parser;
+    }
+
+    /**
+     * @template V
+     * @param V $value
+     * @return Parser<never, V>
+     */
+    final public static function success(mixed $value): Parser
+    {
+        return new Parser\SuccessParser($value);
+    }
+
+    /**
+     * @template T
+     * @return Parser<T, T>
+     */
+    final public static function anyToken(): Parser
+    {
+        static $parser = new Parser\AnyTokenParser();
+
+        return $parser;
+    }
+
+    /**
+     * @template T
+     * @param callable(T $token): bool $predicate
+     * @return Parser<T, T>
+     */
+    final public static function satisfy(callable $predicate): Parser
+    {
+        return new Parser\SatisfyParser($predicate);
+    }
+
+    /**
+     * @template T
+     * @param T $token
+     * @return Parser<T, T>
+     */
+    final public static function token(mixed $token): Parser
+    {
+        return new Parser\TokenParser($token);
+    }
+
+    /**
+     * @template T
+     * @param T ...$tokens
+     * @return Parser<T, T>
+     */
+    final public static function oneOf(mixed ...$tokens): Parser
+    {
+        return new Parser\OneOfParser(...$tokens);
+    }
+
+    /**
+     * @template T
+     * @param T ...$tokens
+     * @return Parser<T, T>
+     */
+    final public static function noneOf(mixed ...$tokens): Parser
+    {
+        return new Parser\NoneOfParser(...$tokens);
+    }
+}
