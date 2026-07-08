18396b4d2e9ed9fbf8303615d1fbad0e7d1c1919
Alex Tsarkov <alex.tsarkov@gmail.com>
2026-07-08 19:54:50 +0300

    fix: phpdocs annotations

--- src/Functor.php
+++ src/Functor.php
@@ -7,7 +7,9 @@ namespace AlexTsarkov\Parsley;
 /**
  * A container that can be mapped over
  *
- * @template Type
+ * @template-covariant Type
+ *
+ * @internal
  */
 interface Functor
 {
@@ -15,7 +17,9 @@ interface Functor
      * Replaces all values with a constant value
      *
      * @template NewType
+     *
      * @param NewType $value
+     *
      * @return self<NewType>
      */
     public function as(mixed $value): self;
@@ -24,7 +28,9 @@ interface Functor
      * Applies a function to the contained values
      *
      * @template NewType
-     * @param callable(Type): NewType $function
+     *
+     * @param callable(Type $x): NewType $function
+     *
      * @return self<NewType>
      */
     public function map(callable $function): self;
