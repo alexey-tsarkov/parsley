59bf1272fd5b6b96014db5cb34960d8ff3afb282
Alex Tsarkov <alex.tsarkov@gmail.com>
2026-07-10 17:21:25 +0300

    chore: add docs

--- src/Parser.php
+++ src/Parser.php
@@ -5,20 +5,29 @@ declare(strict_types=1);
 namespace AlexTsarkov\Parsley;
 
 /**
+ * A parser that parses values from a token stream
+ *
  * @template Token
  * @template Value
+ *
  * @implements Functor<Value>
  */
 abstract readonly class Parser implements Functor
 {
     /**
+     * Attempts to parse a value from the current stream position
+     *
      * @param Stream<Token> $stream
+     *
      * @return ?Result<Value>
      */
     abstract public function parse(Stream $stream): ?Result;
 
     /**
+     * Alias for parse()
+     *
      * @param Stream<Token> $stream
+     *
      * @return ?Result<Value>
      */
     final public function __invoke(Stream $stream): ?Result
@@ -27,8 +36,12 @@ abstract readonly class Parser implements Functor
     }
 
     /**
+     * Replaces the parsed values with a constant
+     *
      * @template NewValue
+     *
      * @param NewValue $value
+     *
      * @return self<Token, NewValue>
      *
      */
@@ -38,8 +51,12 @@ abstract readonly class Parser implements Functor
     }
 
     /**
+     * Transforms the parsed values using a function
+     *
      * @template NewValue
+     *
      * @param callable(Value $value): NewValue $function
+     *
      * @return self<Token, NewValue>
      */
     public function map(callable $function): self
